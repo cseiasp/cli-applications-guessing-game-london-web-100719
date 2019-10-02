ce5de34059e96da1c3c40f37a5900b8bb46690c1
Caroline S-A <caroline.seilern@hotmail.com>
2019-10-02 17:51:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,6 @@ def run_guessing_game
   else if guess == "exit"
     puts "Goodbye!"
   else if guess > "6"
-    puts "Please choose a number between 1 and 6 dumbass"
     run_guessing_game
   else
     puts "Sorry! The computer guessed #{number}."
